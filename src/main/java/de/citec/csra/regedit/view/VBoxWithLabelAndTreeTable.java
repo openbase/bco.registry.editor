de6129fa33368c7bf99aa9b538435399462e85de
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2015-11-25 14:37:29 +0100

    remove some debug logging

--- src/main/java/de/citec/csra/regedit/view/VBoxWithLabelAndTreeTable.java
+++ src/main/java/de/citec/csra/regedit/view/VBoxWithLabelAndTreeTable.java
@@ -22,27 +22,27 @@ import org.slf4j.LoggerFactory;
  * @author <a href="mailto:thuxohl@techfak.uni-bielefeld.com">Tamino Huxohl</a>
  */
 public class VBoxWithLabelAndTreeTable extends VBox {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(VBoxWithLabelAndTreeTable.class);
-    
+
     private final Label readOnlyLabel;
     private final RegistryTreeTableView registryTreeTableView;
     private final SendableType type;
-    
+
     public VBoxWithLabelAndTreeTable(SendableType type) {
         this.type = type;
-        
+
         this.setAlignment(Pos.CENTER);
         readOnlyLabel = new Label("Read-Only-Mode");
         readOnlyLabel.setAlignment(Pos.CENTER);
         readOnlyLabel.setStyle("-fx-text-background-color: rgb(255,128,0); -fx-font-weight: bold;");
-        
+
         registryTreeTableView = new RegistryTreeTableView(type);
         registryTreeTableView.setContextMenu(new TreeTableViewContextMenu(registryTreeTableView, type));
-        
+
         this.getChildren().addAll(registryTreeTableView);
     }
-    
+
     public void setEditable(boolean editable) {
         if (!editable) {
             this.getChildren().clear();
@@ -52,39 +52,37 @@ public class VBoxWithLabelAndTreeTable extends VBox {
         }
         registryTreeTableView.setEditable(editable);
     }
-    
+
     public Label getReadOnlyLabel() {
         return readOnlyLabel;
     }
-    
+
     public RegistryTreeTableView getRegistryTreeTableView() {
         return registryTreeTableView;
     }
-    
+
     public void setRoot(TreeItem<Node> value) {
         registryTreeTableView.setRoot(value);
     }
-    
+
     public void addWidthProperty(ReadOnlyDoubleProperty widthProperty) {
         registryTreeTableView.addWidthProperty(widthProperty);
     }
-    
+
     public void addHeightProperty(ReadOnlyDoubleProperty heightProperty) {
         heightProperty.addListener(new ChangeListener<Number>() {
-            
+
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 registryTreeTableView.setPrefHeight(newValue.doubleValue());
             }
         });
     }
-    
+
     public void setContextMenu(boolean visible) {
         if (visible) {
-            logger.info("added context menu to [" + type + "]");
             registryTreeTableView.setContextMenu(new TreeTableViewContextMenu(registryTreeTableView, type));
         } else {
-            logger.info("removed context menu to [" + type + "]");
             registryTreeTableView.setContextMenu(null);
         }
     }
