e7e84bf3fb671999695f420685584bd581927833
Divine Threepwood <DivineThreepwood@gmail.com>
2015-12-02 11:54:58 +0100

    forward arguments for install script.

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ echo "=== clean ${APP_NAME} ===" &&
 mvn clean &&
 clear &&
 echo "=== install ${APP_NAME} ===" &&
-mvn install &&
+mvn install $@ &&
 clear &&
 echo "=== deploy ${APP_NAME} to ${prefix} ===" &&
-mvn package jar:jar appassembler:assemble -DassembleDirectory=${prefix} &&
+mvn package jar:jar appassembler:assemble -DassembleDirectory=${prefix} $@ &&
 clear &&
 echo "=== ${APP_NAME} is successfully installed to ${prefix} ==="
