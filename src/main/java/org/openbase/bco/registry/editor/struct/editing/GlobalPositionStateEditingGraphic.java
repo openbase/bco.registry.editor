e7f1da76c4423aba2a031a212c56299b7f4e605d
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2018-10-11 17:22:52 +0200

    add global position state tree item for locations

---
+++ src/main/java/org/openbase/bco/registry/editor/struct/editing/GlobalPositionStateEditingGraphic.java
@@ -0,0 +1,74 @@
+package org.openbase.bco.registry.editor.struct.editing;
+
+/*-
+ * #%L
+ * BCO Registry Editor
+ * %%
+ * Copyright (C) 2014 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import javafx.scene.control.TreeTableCell;
+import javafx.scene.layout.HBox;
+import org.openbase.bco.registry.editor.struct.ValueType;
+import org.openbase.bco.registry.editor.struct.editing.util.NumberFilteredTextField;
+import rst.domotic.state.GlobalPositionStateType.GlobalPositionState;
+import rst.domotic.state.GlobalPositionStateType.GlobalPositionState.Builder;
+
+/**
+ * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
+ */
+public class GlobalPositionStateEditingGraphic extends AbstractBuilderEditingGraphic<HBox, GlobalPositionState.Builder> {
+
+    private NumberFilteredTextField longitudeTextField, latitudeTextField, elevationTextField;
+
+    public GlobalPositionStateEditingGraphic(ValueType<Builder> valueType, TreeTableCell<ValueType, ValueType> treeTableCell) {
+        super(new HBox(), valueType, treeTableCell);
+    }
+
+    @Override
+    protected boolean updateBuilder(final Builder builder) {
+        final double newLongitude = longitudeTextField.getAsDouble();
+        final double newLatitude = latitudeTextField.getAsDouble();
+        final double newElevation = elevationTextField.getAsDouble();
+
+        if (newLongitude == builder.getLongitude() && newLatitude == builder.getLatitude() && newElevation == builder.getElevation()) {
+            return false;
+        }
+
+        builder.setLongitude(newLongitude).setLatitude(newLatitude).setElevation(newElevation);
+        return true;
+    }
+
+    @Override
+    protected boolean validate() {
+        return longitudeTextField.validate() && latitudeTextField.validate() && elevationTextField.validate();
+    }
+
+    @Override
+    protected void init(final Builder value) {
+        longitudeTextField = new NumberFilteredTextField();
+        latitudeTextField = new NumberFilteredTextField();
+        elevationTextField = new NumberFilteredTextField();
+
+        getControl().getChildren().addAll(longitudeTextField, latitudeTextField, elevationTextField);
+
+        longitudeTextField.setText(value.getLongitude() + "");
+        latitudeTextField.setText(value.getLatitude() + "");
+        elevationTextField.setText(value.getElevation() + "");
+    }
+}
---
+++ src/main/java/org/openbase/bco/registry/editor/struct/preset/GlobalPositionStateTreeItem.java
@@ -0,0 +1,53 @@
+package org.openbase.bco.registry.editor.struct.preset;
+
+/*-
+ * #%L
+ * BCO Registry Editor
+ * %%
+ * Copyright (C) 2014 - 2018 openbase.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import javafx.scene.Node;
+import javafx.scene.control.TreeTableCell;
+import org.openbase.bco.registry.editor.struct.AbstractBuilderLeafTreeItem;
+import org.openbase.bco.registry.editor.struct.ValueType;
+import org.openbase.bco.registry.editor.struct.editing.GlobalPositionStateEditingGraphic;
+import org.openbase.jul.exception.InitializationException;
+import rst.domotic.state.GlobalPositionStateType.GlobalPositionState;
+import rst.domotic.state.GlobalPositionStateType.GlobalPositionState.Builder;
+
+/**
+ * @author <a href="mailto:pleminoq@openbase.org">Tamino Huxohl</a>
+ */
+public class GlobalPositionStateTreeItem extends AbstractBuilderLeafTreeItem<GlobalPositionState.Builder> {
+
+    public GlobalPositionStateTreeItem(final FieldDescriptor fieldDescriptor, final Builder builder, final Boolean editable) throws InitializationException {
+        super(fieldDescriptor, builder, editable);
+    }
+
+    @Override
+    protected String createValueRepresentation() {
+        return "Longitude = " + getBuilder().getLongitude() + ", Latitude = " + getBuilder().getLatitude() + ", Elevation = " + getBuilder().getElevation();
+    }
+
+    @Override
+    public Node getEditingGraphic(TreeTableCell<ValueType, ValueType> cell) {
+        return new GlobalPositionStateEditingGraphic(getValueCasted(), cell).getControl();
+    }
+}
