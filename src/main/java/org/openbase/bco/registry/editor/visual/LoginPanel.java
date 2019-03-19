12bdd5fe3eee1b78d9165186cd9b5eaf5e236b85
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2019-03-19 21:57:17 +0100

    changes due to authentication module changes

--- src/main/java/org/openbase/bco/registry/editor/visual/LoginPanel.java
+++ src/main/java/org/openbase/bco/registry/editor/visual/LoginPanel.java
@@ -10,12 +10,12 @@ package org.openbase.bco.registry.editor.visual;
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -90,7 +90,7 @@ public class LoginPanel extends HBox implements DynamicPane {
             // load user name from registry if possible.
             String displayedUserName;
             try {
-                displayedUserName = Registries.getUnitRegistry().getUnitConfigById(SessionManager.getInstance().getUserId()).getUserConfig().getUserName();
+                displayedUserName = Registries.getUnitRegistry().getUnitConfigById(SessionManager.getInstance().getUserClientPair().getUserId()).getUserConfig().getUserName();
             } catch (CouldNotPerformException ex) {
                 displayedUserName = userNameTextField.getText();
             }
